74d79cefb20367a52c4f6d5c2b796960e68eab89
ZoraJayKay <126629866+ZoraJayKay@users.noreply.github.com>
2023-05-19 21:16:04 +1000

    Create a to-do list
    Populate list with tasks remaining before minimum viable project is done (incomplete list)

---
+++ Assessment 4/ToDo.h
@@ -0,0 +1,27 @@
+#pragma once
+
+/* Zora's to do list
+* 
+* Instantiate and texture an enemy ship
+* Make enemy ship move left and right
+* When enemy ship collides with edge of screen, reverse direction
+* When enemy ship collides with edge of screen, move closer to player
+* Let enemy instantiate bullets
+* 
+* Instantiate bullets
+* Destroy bullets when they leave screen
+* 
+* Show score
+* Increment score when enemy is destroyed
+* Show lives
+* 
+* Instantiate and texture base
+* Have base destroyed when hit with bullet
+* Have base be partially destroyed when hit with bullet by turning it into array of pixels
+* 256 pixels / maybe 8x8 grid, 32 pixels each? 
+*	Set the texture as an array of boxes either 'on' or 'off, all starting on, turning off permanently when hit by a bullet? If off, bullet can pass through?
+* 
+Populate background
+* 
+
+*/
