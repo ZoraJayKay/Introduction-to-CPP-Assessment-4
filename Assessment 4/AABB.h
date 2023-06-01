a8ab79af89dc3388f60dba462144c90e8fce02d8
ZoraJayKay <126629866+ZoraJayKay@users.noreply.github.com>
2023-06-01 16:56:37 +1000

    AABB changes
    Test adding AABB to its own class, possibly use it through Game Object, possibly through Controller, possibly through Sprite Object...

---
+++ Assessment 4/AABB.h
@@ -0,0 +1,39 @@
+//#pragma once
+//#include "Vector3.h"
+//
+//class AABB
+//{
+//public:
+//	AABB();
+//	~AABB();
+//
+//	// *** BOUNDING BOX VARIABLES ***
+//	float positive = 1;
+//	float negative = -1;
+//
+//	// *** BOUNDING BOX ***
+//	// Axis-Aligned Bounding Box minimum vector3
+//	MyVector3* minVector3AABB = new MyVector3(negative, negative, negative);
+//	// Axis-Aligned Bounding Box maximum vector3
+//	MyVector3* maxVector3AABB = new MyVector3(positive, positive, positive);
+//
+//	// *** BOUNDING BOX FUNCTIONS ***
+//	// Return the centre of the collision hitbox
+//	MyVector3 ReturnCenter();
+//	void SetAABB(MyVector3& min, MyVector3& max);
+//
+//	// Extents
+//	MyVector3 Extents();
+//
+//	// Return the corners of the AABB, using min and max. In 2D we would return 4 corners, and in 3D we would return 8 corners.
+//	MyVector3* Corners();
+//
+//	// Calculate the bounding box for this sprite object
+//	void Fit(MyVector3* points[4]);
+//
+//	/* Test if a point is contained within an AABB by checking if the point is within the range of the min and max corners. */
+//	bool Overlaps(MyVector3& point);
+//
+//	bool Overlaps(SpriteObject& otherSprite);
+//};
+//
--- Assessment 4/SpriteObject.h
+++ Assessment 4/SpriteObject.h
@@ -5,9 +5,9 @@
 class SpriteObject : public GameObject
 {
 public:
-	// *** BOUNDING BOX VARIABLES ***
-	float positive = 1;
-	float negative = -1;
+//	// *** BOUNDING BOX VARIABLES ***
+//	float positive = 1;
+//	float negative = -1;
 
 	//	*** CONSTRUCTOR & DESTRUCTOR
 		// default constructor function
@@ -26,11 +26,11 @@ public:
 		// a new texture for the sprite to use on top of the object
 		Texture2D texture = Texture2D();
 
-		// *** BOUNDING BOX ***
-		// Axis-Aligned Bounding Box minimum vector3
-		MyVector3* minVector3AABB = new MyVector3(negative, negative, negative);
-		// Axis-Aligned Bounding Box maximum vector3
-		MyVector3* maxVector3AABB = new MyVector3(positive, positive, positive);
+		//// *** BOUNDING BOX ***
+		//// Axis-Aligned Bounding Box minimum vector3
+		//MyVector3* minVector3AABB = new MyVector3(negative, negative, negative);
+		//// Axis-Aligned Bounding Box maximum vector3
+		//MyVector3* maxVector3AABB = new MyVector3(positive, positive, positive);
 
 
 	//	*** FUNCTIONS	***
@@ -50,23 +50,23 @@ public:
 	// definition to override the OnDraw from parent GameObject so that the sprite source file can use OnDraw with sprite texture
 	void OnDraw() override;
 
-	// *** BOUNDING BOX FUNCTIONS ***
-	// Return the centre of the collision hitbox
-	MyVector3 ReturnCenter();
-	void SetAABB(MyVector3& min, MyVector3& max);
+	//// *** BOUNDING BOX FUNCTIONS ***
+	//// Return the centre of the collision hitbox
+	//MyVector3 ReturnCenter();
+	//void SetAABB(MyVector3& min, MyVector3& max);
 
-	// Extents
-	MyVector3 Extents();
+	//// Extents
+	//MyVector3 Extents();
 
-	// Return the corners of the AABB, using min and max. In 2D we would return 4 corners, and in 3D we would return 8 corners.
-	MyVector3* Corners();
+	//// Return the corners of the AABB, using min and max. In 2D we would return 4 corners, and in 3D we would return 8 corners.
+	//MyVector3* Corners();
 
-	// Calculate the bounding box for this sprite object
-	void Fit(MyVector3* points[4]);
+	//// Calculate the bounding box for this sprite object
+	//void Fit(MyVector3* points[4]);
 
-	/* Test if a point is contained within an AABB by checking if the point is within the range of the min and max corners. */
-	bool Overlaps(MyVector3& point);
+	///* Test if a point is contained within an AABB by checking if the point is within the range of the min and max corners. */
+	//bool Overlaps(MyVector3& point);
 
-	bool Overlaps(SpriteObject& otherSprite);
+	//bool Overlaps(SpriteObject& otherSprite);
 };
 
